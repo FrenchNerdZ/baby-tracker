b0411640bb18a42b3bb279e0989dcca80553879b
Florian Besnard <florian.besnard@rca.fr>
2025-12-08 23:26:18 +0100

    :card_file_box: ajouter firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,23 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
+import firebase from 'firebase/compat/app';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
+import initializeApp = firebase.initializeApp;
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideZonelessChangeDetection(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyATQtgHRYns0V_ezL-n6jz3vcOfnS2KKjE',
+    authDomain: 'baby-tracker-86a4a.firebaseapp.com',
+    projectId: 'baby-tracker-86a4a',
+    storageBucket: 'baby-tracker-86a4a.firebasestorage.app',
+    messagingSenderId: '438511255422',
+    appId: '1:438511255422:web:c281eea7732252f424bd2f',
+  },
+  production: false,
+};
