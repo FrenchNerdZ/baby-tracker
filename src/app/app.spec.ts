e5f3a5d9a5fefd4666dbe1ccf26e7a04f9f28ac0
Florian Besnard <florian.besnard@rca.fr>
2025-12-08 15:51:12 +0100

    :rotating_light: ajouter prettier et eslint

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed }                          from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { beforeEach, describe, expect, it } from 'vitest';
-import { App }                              from './app';
+import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
